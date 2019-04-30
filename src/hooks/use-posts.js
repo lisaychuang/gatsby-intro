ae41fdf364862aaf4d171b04a33b35b167d3950f
Lisa Huang <lisa.yc.huang@gmail.com>
2019-04-30 12:07:17 -0500

    Add post-preview to homepage

---
+++ src/hooks/use-posts.js
@@ -0,0 +1,28 @@
+import { useStaticQuery, graphql } from 'gatsby';
+
+const usePosts = () => {
+    // use GraphQL query for blog post data
+
+    const data = useStaticQuery(graphql`
+        query {
+            allMdx {
+                nodes {
+                frontmatter {
+                title
+                author
+                }
+                excerpt
+            }
+            }
+        }
+    `)
+
+    return data.allMdx.nodes.map(post => ({
+        title: post.frontmatter.title,
+        author: post.frontmatter.author,
+        slug: post.frontmatter.slug,
+        excerpt: post.excerpt
+    })) 
+}
+
+export default usePosts;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import usePosts from '../hooks/use-posts'
+import PostPreview from '../components/post-preview';
+import ReadLink from '../components/read-link';
 
-export default () => (
+export default () => {
+  const posts = usePosts();
+  return (
     <Layout>
-        <h1>Home</h1>
-        <p>Hello Minnesota!</p>
-        <Link to="/about/">Learn about me &rarr;</Link>
+      <h1>Home</h1>
+      <p>Hello Minnesota!</p>
+      <ReadLink to="/about/">Learn about me &rarr;</ReadLink>
 
+      <h2>Read my blog</h2>
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post} />
+      ))}
     </Layout>
-);
+  );
+};
