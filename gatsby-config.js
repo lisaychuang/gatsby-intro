7edd2575961ac1daaea3ac2ba031660846f2ccaa
Lisa Huang <lisa.yc.huang@gmail.com>
2019-04-30 14:46:29 -0500

    Install webpack-bundle-analyzer

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,5 +32,14 @@ module.exports = {
         path: 'images',
       },
     },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static'
+      }
+    }
   ],
 };
